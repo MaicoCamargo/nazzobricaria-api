16024c655d1e1294ffecd8973f928fb29886788e
Maico Camargo <maicocamargo08@hotmail.com>
2020-04-10 11:44:40 -0300

    chassi projeto- conectado na base de dados

--- src/main/java/com/nazzobricaria/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nazzobricaria.repository;
-
-import com.nazzobricaria.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Produto, Integer> {}
