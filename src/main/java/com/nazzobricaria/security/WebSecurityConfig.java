30b39ae0e6232df65134c8b61c6d2f72eac73b86
Maico Camargo <maicocamargo08@hotmail.com>
2020-04-11 19:28:59 -0300

    classes para realizar autentição e validação de um usuario autenticado

---
+++ src/main/java/com/nazzobricaria/security/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.nazzobricaria.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers(HttpMethod.GET, "/produto").permitAll()
+                .antMatchers(HttpMethod.GET, "/produto/{id}").permitAll()
+                .antMatchers(HttpMethod.GET, "/produto/usuario/{id}").permitAll()
+                .antMatchers(HttpMethod.GET, "/usuario").permitAll()
+                .anyRequest().authenticated()
+                .and()
+
+                // filtra requisições de login
+                .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+
+                // filtra outras requisições para verificar a presença do JWT no header
+                .addFilterBefore(new JWTAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+    }
+
+
+    /*@Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }*/
+}
